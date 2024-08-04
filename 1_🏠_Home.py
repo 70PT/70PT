bcc3945bf53a95decd64182903f27612a17e2e32
70PT <157756721+70PT@users.noreply.github.com>
2024-08-04 23:02:00 +0100

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -196,117 +196,21 @@ with st.container():
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Styles for the slideshow -->
         <style>
-            * {{box-sizing: border-box;}}
-            .mySlides {{display: none;}}
-            img {{vertical-align: middle;}}
-
-            /* Slideshow container */
-            .slideshow-container {{
-            position: relative;
-            margin: auto;
-            width: 100%;
-            }}
-
-            /* The dots/bullets/indicators */
-            .dot {{
-            height: 15px;
-            width: 15px;
-            margin: 0 2px;
-            background-color: #eaeaea;
-            border-radius: 50%;
-            display: inline-block;
-            transition: background-color 0.6s ease;
-            }}
-
-            .active {{
-            background-color: #6F6F6F;
-            }}
-
-            /* Fading animation */
-            .fade {{
-            animation-name: fade;
-            animation-duration: 1s;
-            }}
-
-            @keyframes fade {{
-            from {{opacity: .4}} 
-            to {{opacity: 1}}
-            }}
-
-            /* On smaller screens, decrease text size */
-            @media only screen and (max-width: 300px) {{
-            .text {{font-size: 11px}}
-            }}
             </style>
+        <div>
+        st.text("test")
+        </div>
+        <div>
+        st.text("test")
+        </div>
         </head>
         <body>
-            # <!-- Slideshow container -->
-            # <div class="slideshow-container">
-            #     <div class="mySlides fade">
-            #     <img src={endorsements["img1"]} style="width:100%">
-            #     </div>
 
-            #     <div class="mySlides fade">
-            #     <img src={endorsements["img2"]} style="width:100%">
-            #     </div>
 
-            #     <div class="mySlides fade">
-            #     <img src={endorsements["img3"]} style="width:100%">
-            #     </div>
+        </body>
+        </html> 
 
-            # </div>
-            # <br>
-            # <!-- Navigation dots -->
-            # <div style="text-align:center">
-            #     <span class="dot"></span> 
-            #     <span class="dot"></span> 
-            #     <span class="dot"></span> 
-            # </div>
-
-            # <script>
-            # let slideIndex = 0;
-            # showSlides();
-
-            # function showSlides() {{
-            # let i;
-            # let slides = document.getElementsByClassName("mySlides");
-            # let dots = document.getElementsByClassName("dot");
-            # for (i = 0; i < slides.length; i++) {{
-            #     slides[i].style.display = "none";  
-            # }}
-            # slideIndex++;
-            # if (slideIndex > slides.length) {{slideIndex = 1}}    
-            # for (i = 0; i < dots.length; i++) {{
-            #     dots[i].className = dots[i].className.replace("active", "");
-            # }}
-            # slides[slideIndex-1].style.display = "block";  
-            # dots[slideIndex-1].className += " active";
-            # }}
-
-            # var interval = setInterval(showSlides, 2500); // Change image every 2.5 seconds
-
-            # function pauseSlides(event)
-            # {{
-            #     clearInterval(interval); // Clear the interval we set earlier
-            # }}
-            # function resumeSlides(event)
-            # {{
-            #     interval = setInterval(showSlides, 2500);
-            # }}
-            # // Set up event listeners for the mySlides
-            # var mySlides = document.getElementsByClassName("mySlides");
-            # for (i = 0; i < mySlides.length; i++) {{
-            # mySlides[i].onmouseover = pauseSlides;
-            # mySlides[i].onmouseout = resumeSlides;
-            # }}
-            </script>
-
-            </body>
-            </html> 
-
-            """,
-                height=270,
-    )  
+        """, height=270,)  
 
 # -----------------  contact  ----------------- #
     with col2:
